818482897caf6807f3ae3e37face45e88cb0b18d
zoecq001 <zoecq001@gmail.com>
2019-12-19 11:53:40 +0800

    set up

--- src/App.js
+++ src/App.js
@@ -1,67 +1,26 @@
-import React, { Component } from 'react'
-import Card from './Card.js';
-import './App.css';
-
-// npm start ==> http://localhost:3000/
-
-
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Testimonials from  './components/Testimonials';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
 class App extends Component {
-
-  // 也可以把state写在外面
-  // state = {
-  //   name: 'beastman',
-  //   renderName: 'beastman'
-  // };
-  // constructor(props) {
-  //   super(props);  
-  // }
-
-
-
   render() {
-    // let greeting = `H ${this.state.renderName}!`.split('');
-    // let cards = greeting.map(c => {
-    //   return <Card content={c} />;
-    // });
-
     return (
-     
       <div className="App">
-         
-         {/* the container */}
-          <div className='Card-container'>
-
-            <Card name = "🌟🧭 GT Tour" 
-            description="A web APP developed for Georgia Tech self-guided campus tours"
-            tags = "Web App,React.js,Google Map API"
-            />
-
-            <Card name = "🚀 Space Trader" 
-            description="An Android APP that reintroduces a timeless strategy game"
-            tags = "Android,Native APP,Google Map API"
-            />
-
-            <Card name = "🌟🍜 Yelp Recommend" 
-            description="A restaurant recommendation website based on Yelp API"
-            tags="Web Application,Recommendation System,Java"
-            />
-
-            <Card name = "🃏 Alice in Wonderland" 
-            description="A web based digital artwork"
-            tags = "TypeScript,Node.js"
-            />
-
-            <Card name = "🌟📊 project name" 
-            description="description"
-            />
-
-            <Card name = "🌟🗺️ project name" 
-            description="description"
-            />
-           
-          </div>
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <Testimonials resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
       </div>
     );
-  } 
+  }
 }
+
 export default App;
--- src/tag.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Tag = props => {
-    return (
-        <label className='Tag'>{props.content}</label>
-    );
-}
-
-export default Tag;

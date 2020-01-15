f41ebabfc9403ebce3e7fc80da37af8680efc570
zoecq001 <zoecq001@gmail.com>
2020-01-15 17:45:21 -0500

    add map

--- src/components/Life.js
+++ src/components/Life.js
@@ -8,8 +8,9 @@ export default class Life extends Component {
       return(
         <section id="life">
             <h1 className="title">Life</h1>
-            <h5 className="subtitle">★ See what I am in the reality ★</h5>
+            <h5 className="subtitle">★ See what I do in my spare time ★</h5>
             <hr/>
+            <p><i class="fas fa-suitcase-rolling"></i>  Me as a traveller  <i class="fas fa-plane"></i></p>
             <Map/>
 
 
